d0362d5723ac9d8d890f81266470688f24d9ea9f
agata1309 <agata.orkisz@gmail.com>
2024-09-12 22:21:19 -0500

    next push

--- js/index.js
+++ js/index.js
@@ -1,44 +1,42 @@
-$(" .inline-block").hover(function(e) { 
-      $(this).css("background-color",e.type === "mouseenter"?"lightgrey":"transparent") 
- })
+ 
 
 console.log("Your index.js file is loaded corectly")
 
 
 
  
-$( "li.fade" ).hover(function() {
-  $( this ).fadeOut( 100 );
-  $( this ).fadeIn( 500);
+
+
+
+$(".myImg" ).on ('click', function(){
+  $(".myImg" ).animate({width:"350px"});
 });
 
-$(document).ready(function(){
-  $("p").on({
-    mouseenter: function(){
-      $(this).css("background-color", "white");
-    },  
-    mouseleave: function(){
-      $(this).css("background-color", "lightblue");
-    }, 
-    click: function(){
-      $(this).css("background-color", "cornsilk");
-    }  
-  });
-});
 
-$(document).ready(function(){
-  // Function to animate the image
-  function animateImage() {
-      $('#logo').animate({
-          left: '+=30',    // Move right by 30px
-          top: '+=15',     // Move down by 15px
-          width: '+=10'    // Increase the width by 10px
-      }, 1000, function() {
-          // Reverse the animation to create a continuous gentle movement
-          $('#logo').animate({
-              left: '-=30',
-              top: '-=15',
-              width: '-=10'
-          }, 1000);
-      });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
+}
+
